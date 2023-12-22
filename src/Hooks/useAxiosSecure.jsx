6274048d21f6fd875bb63b6d15bf3e8fe3e228fa
soyebsuvo <soyebbeen@gmail.com>
2023-12-22 19:26:17 +0600

    link changed

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosSecure = axios.create({
-    baseURL : 'http://localhost:5000'
+    baseURL : 'https://job-assignment-server.vercel.app'
 })
 export default function useAxiosSecure() {
   return axiosSecure;
